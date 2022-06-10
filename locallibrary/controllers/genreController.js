e0d7cedc2290e67fde6a54f38a960c4b7a3e817b
Daina Viliunaite <daina06vil@yandex.ru>
2022-06-10 14:33:52 +0300

    task 13

--- locallibrary/controllers/genreController.js
+++ locallibrary/controllers/genreController.js
@@ -1,33 +1,63 @@
- var Genre = require('../models/genre');
-  // Показать список всех Genre.
-  exports.genre_list = function(req, res) {
-      res.send('NOT IMPLEMENTED: Genre list');
+var Genre = require('../models/genre');
+var Book = require('../models/book');
+var async = require('async');
+var mongoose = require('mongoose');
+// Показать список всех Genre.
+exports.genre_list = function(req, res) {
+    Genre.find()
+        .exec(function (err, list_genres) {
+      if (err) { return next(err); } //Successful, so render 
+      res.render('genre_list', {
+              title: 'Genre List', genre_list: list_genres
+           });
+}); };
+// Показать подробную страницу для данного Genre.
+exports.genre_detail = function(req, res, next) {
+    var id = mongoose.Types.ObjectId(req.params.id);
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+            .exec(callback);
+        },
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id })
+            .exec(callback);
+        },
+    }, function(err, results) {
+          if (err) { return next(err); }
+          if (results.genre==null) { // No results.
+              var err = new Error('Genre not found');
+              err.status = 404;
+              return next(err);
+          }
+          res.render('genre_detail', {
+              title: 'Genre Detail', genre: results.genre,
+                genre_books: results.genre_books
+          });
+    }); 
 };
-  // Показать подробную страницу для данного Genre.
-  exports.genre_detail = function(req, res) {
-      res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+
+// Показать форму создания Genre по запросу GET.
+exports.genre_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre create GET');
 };
-  // Показать форму создания Genre по запросу GET.
-  exports.genre_create_get = function(req, res) {
-      res.send('NOT IMPLEMENTED: Genre create GET');
+// Создать Genre по запросу POST.
+exports.genre_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre create POST');
 };
-  // Создать Genre по запросу POST.
-  exports.genre_create_post = function(req, res) {
-      res.send('NOT IMPLEMENTED: Genre create POST');
+// Показать форму удаления Genre по запросу GET.
+exports.genre_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre delete GET');
 };
-  // Показать форму удаления Genre по запросу GET.
-  exports.genre_delete_get = function(req, res) {
-      res.send('NOT IMPLEMENTED: Genre delete GET');
+// Удалить Genre по запросу POST.
+exports.genre_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre delete POST');
 };
-  // Удалить Genre по запросу POST.
-  exports.genre_delete_post = function(req, res) {
-      res.send('NOT IMPLEMENTED: Genre delete POST');
-};
-  // Показать форму обновления Genre по запросу GET.
-  exports.genre_update_get = function(req, res) {
-      res.send('NOT IMPLEMENTED: Genre update GET');
+// Показать форму обновления Genre по запросу GET.
+exports.genre_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre update GET');
 };
 // Обновить Genre по запросу POST.
-  exports.genre_update_post = function(req, res) {
-      res.send('NOT IMPLEMENTED: Genre update POST');
+exports.genre_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre update POST');
 };
